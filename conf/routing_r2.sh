8fb014c558506568b668fb368497c5c7c8f56f2f
Louis Royer <infos.louis.royer@gmail.com>
2023-04-20 10:14:16 +0200

    Use seg6local everywhere

--- conf/routing_r2.sh
+++ conf/routing_r2.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
 # Add sid
+ip sr tunsrc set fd00:51D5:0000:2::
 ip link add sid type dummy
 ip link set dev sid up
-ip -6 addr add fd00:51D5:0000:2:1::/128 dev sid # End
+echo 100 localsid >> /etc/iproute2/rt_tables
+ip -6 rule add to fd00:51D5:0000:2::/64 lookup localsid
+ip -6 route add blackhole default table localsid
+ip -6 route add fd00:51D5:0000:2:1::/80 encap seg6local action End dev sid table localsid proto static
 
 # Add nei sid
 ip -6 route add fd00:51D5:0000:1::/64 via fd00:d0cc:e700:1111:12::1 dev n12-0 proto static
