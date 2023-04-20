7041da9949d092459395a7f744daa6416f523a17
Louis Royer <infos.louis.royer@gmail.com>
2023-04-20 09:56:20 +0200

    /80 for localsid routes

--- conf/routing_r4.sh
+++ conf/routing_r4.sh
@@ -7,7 +7,7 @@ ip -6 addr add fd00:51D5:0000:4:1::/128 dev sid # End
 echo 100 localsid >> /etc/iproute2/rt_tables
 ip -6 rule add to fd00:51D5:0000:4::/64 lookup localsid
 ip -6 route add blackhole default table localsid
-ip -6 route add fd00:51D5:0000:4:11::/128 encap seg6local action End.DX4 nh4 0.0.0.0 dev target-0 table localsid proto static
+ip -6 route add fd00:51D5:0000:4:11::/80 encap seg6local action End.DX4 nh4 0.0.0.0 dev target-0 table localsid proto static
 
 # Add nei sid
 ip -6 route add fd00:51D5:0000:2::/64 via fd00:d0cc:e700:1111:24::2 dev n24-0 proto static
