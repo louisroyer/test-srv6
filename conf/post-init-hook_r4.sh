690f1993e36a505b27f7ac2c8b0c5d6f975dd9e7
Louis Royer <infos.louis.royer@gmail.com>
2023-06-01 18:17:45 +0200

    Debug sr to gtp

--- conf/post-init-hook_r4.sh
+++ conf/post-init-hook_r4.sh
@@ -5,4 +5,5 @@ ip -6 route add fd00:51D5:0000:2::/64 via fd00:d0cc:e700:1111:24::2 dev n24-0 pr
 ip -6 route add fd00:51D5:0000:3::/64 via fd00:d0cc:e700:1111:34::3 dev n34-0 proto static
 
 # Add sr route
+ip sr tunsrc set fd00:51D5:0000:4::
 ip route add 10.0.1.0/24 encap seg6 mode encap segs fd00:51D5:0000:2:1::,fd00:51D5:0000:1:11:: dev linux-sr proto static
