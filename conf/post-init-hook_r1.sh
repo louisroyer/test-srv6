7fb3f524e409391f6b0033b31beecd827c3edf5b
Louis Royer <infos.louis.royer@gmail.com>
2023-06-02 10:07:23 +0200

    GTP to SR

--- conf/post-init-hook_r1.sh
+++ conf/post-init-hook_r1.sh
@@ -6,4 +6,4 @@ ip -6 route add fd00:51D5:0000:3::/64 via fd00:d0cc:e700:1111:13::3 dev n13-0 pr
 
 # Add sr route
 ip sr tunsrc set fd00:51D5:0000:1::
-ip route add 10.0.2.0/24 encap seg6 mode encap segs fd00:51D5:0000:2:69:0012:3456:7800,fd00:51D5:0000:4:11:: dev linux-sr proto static
+ip route add 10.0.2.0/24 encap seg6 mode encap segs fd00:51D5:0000:2:69:0012:3456:7800,fd00:51D5:0000:4:71:: dev linux-sr proto static
