28897de044903cd9feafbdd6f79c7614dc99f6cc
Louis Royer <infos.louis.royer@gmail.com>
2023-06-09 20:58:07 +0200

    Tests GTP4

--- conf/post-init-hook_r1.sh
+++ conf/post-init-hook_r1.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 # Add nei sid
-ip -6 route add fd00:51D5:0000:2::/64 via fd00:d0cc:e700:1111:12::2 dev n12-0 proto static
+ip -6 route add fd00:51D5:0002::/48 via fd00:d0cc:e700:1111:12::2 dev n12-0 proto static
 ip -6 route add fd00:51D5:0000:3::/64 via fd00:d0cc:e700:1111:13::3 dev n13-0 proto static
 
 # Add sr route
 ip sr tunsrc set fd00:51D5:0000:1::
-ip route add 10.0.2.0/24 encap seg6 mode encap segs fd00:51D5:0000:2:69:0012:3456:7800,fd00:51D5:0000:4:71:: dev linux-sr proto static
+ip route add 10.0.2.0/24 encap seg6 mode encap segs fd00:51D5:0002:050a:004e:0200:1234:5678 dev linux-sr proto static
+# 10.0.78.2 to hex = 0A.00.4E.02
--- conf/post-init-hook_r3.sh
+++ conf/post-init-hook_r3.sh
@@ -2,5 +2,5 @@
 
 # Add nei sid
 ip -6 route add fd00:51D5:0000:1::/64 via fd00:d0cc:e700:1111:13::1 dev n13-0 proto static
-ip -6 route add fd00:51D5:0000:2::/64 via fd00:d0cc:e700:1111:23::2 dev n23-0 proto static
+ip -6 route add fd00:51D5:0002::/48 via fd00:d0cc:e700:1111:23::2 dev n23-0 proto static
 ip -6 route add fd00:51D5:0000:4::/64 via fd00:d0cc:e700:1111:55::4 dev ninter-0 proto static
